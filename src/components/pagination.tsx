3b6e044f4964143cdfb7a4f1f31b6e8f59023b05
Rumen Stankov <rumen.stankov@gmail.com>
2022-05-18 13:21:02 +0300

    Add filtering examples

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -3,7 +3,6 @@ import { withStyles } from "@material-ui/core/styles";
 import { IconButton, Tooltip, Typography } from "@mui/material";
 import { merge } from 'lodash'
 
-
 function MTablePaginationInner(props: any) {
   const {
     classes,
@@ -15,16 +14,9 @@ function MTablePaginationInner(props: any) {
     onChangePage,
   } = props;
 
-  // const localization = {
-  //   ...defaultProps.localization,
-  //   ...props.localization,
-  // };
-
   const localization = merge(defaultProps.localization, props.localization);
 
-
-
-  console.log(props.localization)
+  //const localization = defaultProps.localization;
 
   const handleFirstPageButtonClick = (event: any) => {
     onChangePage(event, 0);
